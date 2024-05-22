461a5ec76162995467be16f936a58b164d005f3c
Javier Gonzalez <javgon2306@gmail.com>
2024-05-22 00:09:25 -0300

    alt chagnes

--- react-01/src/01-useState/CounterApp.jsx
+++ react-01/src/01-useState/CounterApp.jsx
@@ -7,9 +7,11 @@ export const CounterApp = () =>{
     
     return (
         <>
-            <h1>Counter1: { counter1 }</h1>
-            <hr />
-            <button className="btn border-primary"  onClick={ () => setCounter( counter1 + 1) }>+1</button>
+            <div>
+                <h1>Counter1: { counter1 }</h1>
+                <hr />
+                <button className="btn border-primary"  onClick={ () => setCounter( counter1 + 1) }>+1</button>
+            </div>
         </>
     )
 }
